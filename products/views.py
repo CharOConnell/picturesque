81512fb860778c89298bcedd9442ea02e998ccbd
CharOConnell <charlotteoconnell@virginmedia.com>
2020-11-04 13:56:57 +0000

    Added product details page

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
 
@@ -7,14 +7,24 @@ def all_products(request):
     including sorting and search queries """
 
     products = Product.objects.all()
-    categories = Category.objects.all()
-    
+
     context = {
         'products': products,
-        'categories': categories,
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show selected product information """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
 
 
 def collections(request):
@@ -28,9 +38,19 @@ def collections(request):
         'na090-028',
     ]
 
+    links = [{'id': 285, 'link': 'products'}]
+    """
+        {'id': 17, 'link': 'architecture'},
+        {'id': 82, 'link': 'castles'},
+        {'id': 129, 'link': 'vehicles'},
+        {'id': 176, 'link': 'landscapes'},
+        {'id': 267, 'link': 'nature'}
+    ]"""
+
     context = {
         'products': products,
         'skus': skus,
+        'links': links,
     }
     template = 'products/collections.html'
 
