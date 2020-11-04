b1f53c7694b31ab101388c630fc7f5e647190028
CharOConnell <charlotteoconnell@virginmedia.com>
2020-11-04 12:27:22 +0000

    Added collections page

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 
 
 def all_products(request):
@@ -7,9 +7,31 @@ def all_products(request):
     including sorting and search queries """
 
     products = Product.objects.all()
-
+    categories = Category.objects.all()
+    
     context = {
         'products': products,
+        'categories': categories,
     }
 
     return render(request, 'products/products.html', context)
+
+
+def collections(request):
+    products = Product.objects.all()
+    skus = [
+        'na100-046',
+        'ar010-017',
+        'ca020-026',
+        've030-016',
+        'la060-044',
+        'na090-028',
+    ]
+
+    context = {
+        'products': products,
+        'skus': skus,
+    }
+    template = 'products/collections.html'
+
+    return render(request, template, context)
