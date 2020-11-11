010368ed371692e265f050b6e1a41ff152bcab9e
CharOConnell <charlotteoconnell@virginmedia.com>
2020-11-11 15:54:48 +0000

    Added checkout form data caching to payment intent

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -7,8 +7,8 @@
     https://stripe.com/docs/stripe-js
 */
 
-var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 // sliced to remove quotation marks
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
@@ -54,59 +54,79 @@ var form = document.getElementById('payment-form')
 
 form.addEventListener('submit', function(ev) {
     ev.preventDefault();
+    // prevent the button being pressed whilst processing
     card.update({'disabled': true});
     $('#submit-button').attr('disabled', true);
-    // prevent the button being pressed whilst processing
+    
+    // display loading overlay
     $('#payment-form').fadeToggle(100);
     $('#loading-overlay').fadeToggle(100);
-    stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: card,
-            billing_details: {
+
+    // add data to the payment intent (save info, username, etc)
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    };
+    var url = '/checkout/cache_checkout_data/';
+
+    // post data to the payment intent before passing to stripe
+    $.post(url, postData).done(function() {
+        // pass the data to Strip in the payment intent
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: $.trim(form.full_name.value),
+                    phone: $.trim(form.phone_number.value),
+                    email: $.trim(form.email.value),
+                    address:{
+                        line1: $.trim(form.street_address1.value),
+                        line2: $.trim(form.street_address2.value),
+                        city: $.trim(form.town.value),
+                        country: $.trim(form.country.value),
+                        state: $.trim(form.county.value),
+                    }
+                }
+            },
+            shipping: {
                 name: $.trim(form.full_name_value),
                 phone: $.trim(form.phone_number.value),
-                email: $.trim(form.email.value),
                 address:{
                     line1: $.trim(form.street_address1.value),
                     line2: $.trim(form.street_address2.value),
                     city: $.trim(form.town.value),
                     state: $.trim(form.county.value),
+                    postal_code: $.trim(form.postcode.value),
                     country: $.trim(form.country.value),
                 }
-            }
-        },
-        shipping: {
-            name: $.trim(form.full_name_value),
-            phone: $.trim(form.phone_number.value),
-            address:{
-                line1: $.trim(form.street_address1.value),
-                line2: $.trim(form.street_address2.value),
-                city: $.trim(form.town.value),
-                state: $.trim(form.county.value),
-                postal_code: $.trim(form.postcode.value),
-                country: $.trim(form.country.value),
-            }
-        },
-    }).then(function(result) {
-        if (result.error) {
-            var errorDiv = document.getElementById('card-errors');
-            var html = `
-                <span class="icon">
-                    <i class="fas fa-times"></i>
-                </span>
-                <span>${result.error.message}</span>
-            `;
-            $(errorDiv).html(html);
-            // Allow user to fix error and re-submit
-            $('#payment-form').fadeToggle(100);
-            $('#loading-overlay').fadeToggle(100);
-            card.update({'disabled': false});
-            $('#submit-button').attr('disabled', false);
-        } else {
-            // The payment has been processed
-            if (result.paymentIntent.status === 'succeeded') {
-                form.submit();
+            },
+        }).then(function(result) {
+            if (result.error) {
+                var errorDiv = document.getElementById('card-errors');
+                var html = `
+                    <span class="icon">
+                        <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>
+                `;
+                $(errorDiv).html(html);
+                // Allow user to fix error and re-submit
+                $('#payment-form').fadeToggle(100);
+                $('#loading-overlay').fadeToggle(100);
+                card.update({'disabled': false});
+                $('#submit-button').attr('disabled', false);
+            } else {
+                // The payment has been processed
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
+                };
             };
-        };
+        });
+    }).fail(function() {
+        // load the page again to show the error message if it fails without charging user
+        location.reload();
     });
 });
