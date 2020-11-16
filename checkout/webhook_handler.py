df2653ebd5978462246429264ac0a64fc67fc482
CharOConnell <charlotteoconnell@virginmedia.com>
2020-11-16 12:44:15 +0000

    Added email confirmations

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 from .models import Order, OrderLineItem
 from products.models import Product
@@ -13,6 +16,22 @@ class Stripe_WH_Handler:
 
     def __init__(self, request):
         self.request = request
+
+    def _send_confirmation_email(self, order):
+        """ Send an email confirmation to the user """
+        customer_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [customer_email]
+        )
 
     def handle_event(self, event):
         """ Handle a generic/unknown/unexpected webhook event """
@@ -78,6 +97,8 @@ class Stripe_WH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            # send confirmation email
+            self._send_confirmation_email(order)
             # if it has been set to true as we have an order
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
@@ -116,6 +137,8 @@ class Stripe_WH_Handler:
                     content=f'Webhoook received: {event["type"]} | ERROR: {e}',
                     status=500)
 
+        # send confirmation email
+        self._send_confirmation_email(order)
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
