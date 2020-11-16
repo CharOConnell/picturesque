907a20309c32e9916c7ea0195d96c90c1ee367ff
CharOConnell <charlotteoconnell@virginmedia.com>
2020-11-16 16:41:00 +0000

    Added an edit product view and template

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('collections/', views.collections, name='collections'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
