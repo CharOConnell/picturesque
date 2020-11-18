fcd1e3afc4a7e56f2795a0717343e9a1a1fb692f
CharOConnell <charlotteoconnell@virginmedia.com>
2020-11-18 15:10:08 +0000

    Added accordion of FAQs to contact page and styled

--- contact/views.py
+++ contact/views.py
@@ -13,17 +13,20 @@ def contact(request):
         email_address_customer = request.POST['email']
         email_subject = request.POST['subject']
         email_message = request.POST['message_content']
-
+        # Formulate a body string
         body = f'An e-mail from {email_name}: \n {email_message}'
 
+        # Send an e-mail using Django's send_mail function
         send_mail(
             email_subject,
             body,
             email_address_customer,
             [settings.DEFAULT_FROM_EMAIL]
         )
-
+        # Let the user know an email has successfully been sent
         messages.success(request, f'Thank you {email_name} for your e-mail.')
+        # Refresh the page with an empty form
         return redirect(reverse('contact'))
     else:
+        # No form submission, render the contact page
         return render(request, 'contact/contact.html')
